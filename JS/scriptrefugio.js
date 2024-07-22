016833ba084ba27f3cbab4c43afdbe9e1a5cb9d1
Jorge de María <jorgedemaria@pm.me>
2024-07-21 22:50:43 -0400

    último push

--- JS/scriptrefugio.js
+++ JS/scriptrefugio.js
@@ -73,4 +73,16 @@ Array.from(galeria_imagenes).forEach(img => {
 
 )
 
+// TOGGLER 
 
+const boton = document.getElementById("plus")
+console.log(boton)
+document.getElementById("plus").addEventListener("click", function(){
+    document.getElementById("remove").style.display = "initial"
+    document.getElementById("plus").style.display = "none"
+})
+
+document.getElementById("remove").addEventListener("click", function(){
+    document.getElementById("remove").style.display = "none"
+    document.getElementById("plus").style.display = "initial"
+})
